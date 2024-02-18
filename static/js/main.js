86e1eedf9598b576391037eaaa9cbdb29be2433d
granawkins <granthawkins88@gmail.com>
2024-02-18 12:59:52 +0700

    add function calls as arrows

--- static/js/main.js
+++ static/js/main.js
@@ -15,12 +15,26 @@ camera.position.y = 2;
 camera.position.z = 5;
 
 // Render nodes based on coordinates
+node_radius = 0.05
 nodes.forEach(node => {
-    const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+    const geometry = new THREE.SphereGeometry(node_radius, 32, 32);
     const material = new THREE.MeshBasicMaterial({ color: "white" });
     const sphere = new THREE.Mesh(geometry, material);
     sphere.position.set(node.x, node.y, node.z);
     scene.add(sphere);
+});
+// Render links as arrows
+links.forEach(link => {
+    const sourceNode = nodes.find(node => node.id === link.source);
+    const targetNode = nodes.find(node => node.id === link.target);
+
+    if (sourceNode && targetNode) {
+        const dir = new THREE.Vector3(targetNode.x - sourceNode.x, targetNode.y - sourceNode.y, targetNode.z - sourceNode.z);
+        const length = dir.length() - node_radius;
+        dir.normalize();
+        const arrowHelper = new THREE.ArrowHelper(dir, new THREE.Vector3(sourceNode.x, sourceNode.y, sourceNode.z), length, "lime", 0.1, 0.05);
+        scene.add(arrowHelper);
+    }
 });
 
 // Animation loop
