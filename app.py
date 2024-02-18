ed491b62ef4ab4ed688e4461127157fcaa265a3a
granawkins <granthawkins88@gmail.com>
2024-02-18 11:30:43 +0700

    add multifile_calculator, networkx and app skeleton

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+from pathlib import Path
+
+import networkx as nx
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def generate_call_graph(path: Path) -> nx.MultiDiGraph:
+    pass
+
 @app.route('/')
 def home():
+    graph = generate_call_graph(Path('test.py'))
+    print(graph)
+    # Generate a call graph
+    # Generate coordinates
+    # Graph them
     return render_template('index.html')
 
 if __name__ == '__main__':
