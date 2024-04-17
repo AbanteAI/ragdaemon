8f8b76873ecc0c3ce9ae7389066d112f69183188
granawkins <granthawkins88@gmail.com>
2024-04-17 11:45:20 +0700

    add persmissions to tests on windows

--- tests/test_get_paths.py
+++ tests/test_get_paths.py
@@ -1,3 +1,6 @@
+import gc
+import os
+import stat
 import shutil
 from pathlib import Path
 
@@ -21,11 +24,33 @@ def test_get_paths_for_directory_git(cwd):
     }, "Paths are not equal"
 
 
+def add_permissions(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is because the file is being used by another process,
+    it retries after a short delay.
+
+    If the error is for another reason it re-raises the error.
+    """
+
+    gc.collect()  # Force garbage collection
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
+
+
 def test_get_paths_for_directory_without_git(git_history):
     # Using the 'git_history' fixture because it sets up a tempdir.
     git_history = git_history.resolve()
     git_dir = git_history / ".git"
-    shutil.rmtree(git_dir)
+    shutil.rmtree(git_dir, onerror=add_permissions)
 
     is_git_repo = get_git_root_for_path(git_history, raise_error=False)
     assert not is_git_repo, "Is a git repository"
