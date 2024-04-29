2c4ca5e63c3d5d2bf4533c5d5755c0f7f2c7d30c
granawkins <granthawkins88@gmail.com>
2024-04-29 11:31:31 -0500

    implement batch chunker_llm

--- ragdaemon/annotators/chunker_llm.py
+++ ragdaemon/annotators/chunker_llm.py
@@ -33,76 +33,72 @@ class ChunkerLLM(Chunker):
 
     async def get_llm_response(
         self,
-        file_message: str,
+        file: str,
+        file_lines: list[str],
         last_chunk: Optional[dict[str, Any]] = None,
-        depth: int = 0,
     ) -> List[Dict[str, Any]]:
-        if depth > 25:
-            raise RagdaemonError("Maximum recursion depth reached for chunker.")
+        """Get one chunking response from the LLM model."""
         if self.spice_client is None:
             raise RagdaemonError("Spice client is not initialized.")
+        
+        messages = SpiceMessages(self.spice_client)
+        messages.add_system_prompt(name="chunker_llm.base")
+        if last_chunk is not None:
+            messages.add_system_prompt("chunker_llm.continuation", last_chunk=last_chunk)
+        messages.add_user_prompt("chunker_llm.user", path=file, code="\n".join(file_lines))
+
         global semaphore
         async with semaphore:
-            messages = SpiceMessages(self.spice_client)
-            messages.add_system_prompt(name="chunker_llm.base")
-            if last_chunk == None:
-                messages.add_system_prompt(name="chunker_llm.first_pass")
-            else:
-                messages.add_system_prompt(
-                    name="chunker_llm.next_pass",
-                    last_chunk=json.dumps(last_chunk, indent=4),
-                )
-            messages.add_user_message(file_message)
-
             response = await self.spice_client.get_response(
                 messages=messages,
                 response_format={"type": "json_object"},
             )
-            try:
-                return json.loads(response.text)["chunks"]
-            except JSONDecodeError:
-                # This probably means the output is too long, i.e. there are too many
-                # functions to chunk in one pass. We parse what we can and then recall to finish the job.
-                index = response.text.rfind("}")
-                fixed_json = response.text[: index + 1] + "]}"
-                chunks = json.loads(fixed_json)["chunks"]
-                new_chunks = await self.get_llm_response(
-                    file_message, last_chunk=chunks[-1], depth=depth + 1
+        try:
+            chunks = json.loads(response.text)["chunks"]
+        except JSONDecodeError:
+            raise RagdaemonError(
+                "Failed to parse JSON response. This could mean that the output is too "
+                "long, i.e. there are too many functions to chunk in one pass. If this "
+                "is the case, decrease the batch size and try again."
+            )
+        for chunk in chunks:
+            if not is_chunk_valid(chunk):
+                raise RagdaemonError(f"Model returned chunk: {chunk}")
+        if last_chunk is not None:
+            if not any(chunk["id"] == last_chunk["id"] for chunk in chunks):
+                raise RagdaemonError(
+                    f"Last chunk replacement ({last_chunk['id']}) not found in response."
                 )
-                seen = set(chunk["id"] for chunk in chunks)
-                for chunk in new_chunks:
-                    if chunk["id"] not in seen:
-                        chunks.append(chunk)
-                        seen.add(chunk["id"])
-                return chunks
+        return chunks
 
-    async def chunk_document(self, document: str) -> list[dict[str, Any]]:
+    async def chunk_document(self, document: str, batch_size: int = 1000, retries: int = 1) -> list[dict[str, Any]]:
         """Parse file_lines into a list of {id, ref} chunks."""
         lines = document.splitlines()
         file = lines[0]
         file_lines = lines[1:]
         if not file_lines or not any(line for line in file_lines):
             return []
+        file_lines = [f"{i+1}:{line}" for i, line in enumerate(file_lines)]
 
         # Get raw llm output
         chunks = list[dict[str, Any]]()
-        tries: int = 1
-        for tries in range(tries, 0, -1):
-            tries -= 1
-            numbered_lines = "\n".join(
-                f"{i+1}:{line}" for i, line in enumerate(file_lines)
-            )
-            file_message = f"{file}\n{numbered_lines}"
-            _chunks = await self.get_llm_response(file_message)
-            if not _chunks or all(is_chunk_valid(chunk) for chunk in _chunks):
-                chunks = _chunks
-                break
-            if tries > 1 and self.verbose:
-                print(f"Error with chunker response:\n{chunks}.\n{tries} tries left.")
-        if not chunks:
-            return []
-        if not all(is_chunk_valid(chunk) for chunk in chunks):
-            raise RagdaemonError(f"Invalid chunk data: {chunks}")
+        n_batches = (len(file_lines) + batch_size - 1) // batch_size
+        for i in range(n_batches):
+            batch_lines = file_lines[i * batch_size : (i + 1) * batch_size]
+            last_chunk = chunks.pop() if chunks else None
+            for j in range(retries + 1, 0, -1):
+                try:
+                    _chunks = await self.get_llm_response(file, batch_lines, last_chunk)
+                    chunks.extend(_chunks)
+                    break
+                except RagdaemonError as e:
+                    if self.verbose:
+                        print(
+                            f"Error chunking {file} batch {i+1}/{n_batches}:\n{e}\n"
+                            + f"{j-1} retries left." if j > 1 else "Skipping."
+                        )
+                    if j == 1:
+                        return []
 
         # Generate a 'BASE chunk' with all lines not already part of a chunk
         base_chunk_lines = set(range(1, len(file_lines) + 1))
